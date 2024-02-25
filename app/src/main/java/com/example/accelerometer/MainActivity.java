6d713370d31e391a79b5f07e3b4b1d71f53d220b
AustinDayDay <iv23829@bristol.ac.uk>
2024-02-25 17:49:55 +0000

    Initial commit

---
+++ app/src/main/java/com/example/accelerometer/MainActivity.java
@@ -0,0 +1,68 @@
+package com.example.accelerometer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorManager;
+import android.hardware.SensorEventListener;
+import android.os.Vibrator;
+import android.os.VibrationEffect;
+import android.view.View;
+import android.widget.TextView;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity implements SensorEventListener{
+
+    private TextView xText, yText, zText;
+    private Sensor mySensor;
+    private SensorManager SM;
+    private Vibrator vibrator;
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        xText.setText("X " + Math.round(event.values[0]));
+        yText.setText("Y " + Math.round(event.values[1]));
+        zText.setText("Z " + Math.round(event.values[2]));
+
+        if(event.values[0] < 6) {
+            vibrator.vibrate(1000);
+
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+        //not in use
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //vibrator
+        vibrator = (Vibrator) getSystemService(VIBRATOR_SERVICE);
+
+        //Create our Sensor Manager
+        SM = (SensorManager) getSystemService(SENSOR_SERVICE);
+
+        //Accelerometer Sensor
+        mySensor = SM.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+
+        //Register sensor listener
+        SM.registerListener(this, mySensor, SensorManager.SENSOR_DELAY_NORMAL);
+
+        //Assign TextView
+        xText = (TextView) findViewById(R.id.xText);
+        yText = (TextView) findViewById(R.id.yText);
+        zText = (TextView) findViewById(R.id.zText);
+    }
+
+    private void startForegroundService(){
+
+
+    }
+}
